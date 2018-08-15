f7ccd2f6d72eacbc6bb20884c05a08e77df9c6b8
Mifort <themifort@gmail.com>
2018-08-16 02:47:09 +0300

    Group update models(exclude change settings model)

---
+++ VkNet/Model/GroupUpdate/UserBlock.cs
@@ -0,0 +1,55 @@
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using VkNet.Enums;
+using VkNet.Utils;
+
+namespace VkNet.Model.GroupUpdate
+{
+	/// <summary>
+	/// Добавление пользователя в чёрный список
+	/// </summary>
+	[Serializable]
+	public class UserBlock
+	{
+		/// <summary>
+		/// Идентификатор пользователя
+		/// </summary>
+		[JsonProperty("user_id")]
+		public long? UserId { get; set; }
+
+		/// <summary>
+		/// Идентификатор администратора, который внёс пользователя в чёрный список
+		/// </summary>
+		[JsonProperty("admin_id")]
+		public long? AdminId { get; set; }
+
+		/// <summary>
+		/// Дата разблокировки
+		/// </summary>
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		[JsonProperty("unblock_date")]
+		public DateTime? UnblockDate { get; set; }
+
+		/// <summary>
+		/// Причины блокировки пользователя
+		/// </summary>
+		[JsonProperty("reason")]
+		public GroupUserBlockReason? Reason { get; set; }
+
+		/// <summary>
+		/// Комментарий администратора к блокировке
+		/// </summary>
+		[JsonProperty("comment")]
+		public string Comment { get; set; }
+
+		/// <summary>
+		/// Разобрать из json.
+		/// </summary>
+		/// <param name="response"> Ответ сервера. </param>
+		public static UserBlock FromJson(VkResponse response)
+		{
+			return JsonConvert.DeserializeObject<UserBlock>(response.ToString());
+		}
+	}
+}
